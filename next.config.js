8c4c68f2c4e01c3346bf6626199c5fceda3c5e82
Jérôme Velociter <jerome@velociter.fr>
2020-01-18 19:04:56 +0100

    Rewrite / to /home + add prettier hook

--- next.config.js
+++ next.config.js
@@ -1,30 +1,32 @@
-require('dotenv').config();
-const path = require('path');
+require("dotenv").config();
+const path = require("path");
 
 module.exports = {
-    env: {
-        PRISMIC_REPOSITORY: process.env.PRISMIC_REPOSITORY
-    },
+  env: {
+    PRISMIC_REPOSITORY: process.env.PRISMIC_REPOSITORY
+  },
 
-    webpack(config, options) {
-        config.module.rules.push({
-            test: /\.graphql$/,
-            include: [
-                path.resolve(__dirname, "queries")
-            ],
-            exclude: /node_modules/,
-            use: [options.defaultLoaders.babel, { loader: 'graphql-let/loader' }],
-        });
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.graphql$/,
+      include: [path.resolve(__dirname, "queries")],
+      exclude: /node_modules/,
+      use: [options.defaultLoaders.babel, { loader: "graphql-let/loader" }]
+    });
 
-        config.module.rules.push({
-            test: /\.graphql$/,
-            include: [
-                path.resolve(__dirname, "schema")
-            ],
-            exclude: /node_modules/,
-            loader: 'graphql-tag/loader',
-        });
+    config.module.rules.push({
+      test: /\.graphql$/,
+      include: [path.resolve(__dirname, "schema")],
+      exclude: /node_modules/,
+      loader: "graphql-tag/loader"
+    });
 
-        return config
-    },
+    return config;
+  },
+
+  experimental: {
+    async rewrites() {
+      return [{ source: "/", destination: "/home" }];
+    }
+  }
 };
